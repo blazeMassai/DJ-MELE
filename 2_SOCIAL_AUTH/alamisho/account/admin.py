918a0d019c9099da06cc131441716573e93881fc
unknown <reward.masalla@trc.co.tz>
2023-01-08 23:08:34 +0300

    start messages page 219 08-01-2023

--- 2_SOCIAL_AUTH/alamisho/account/admin.py
+++ 2_SOCIAL_AUTH/alamisho/account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profiles
+
 
 # Register your models here.
+@admin.register(Profiles)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
+    raw_id_fields = ['user']
