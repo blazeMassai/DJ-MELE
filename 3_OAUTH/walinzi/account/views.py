18e0544cc10a0628d6170435419d9768e7729e9e
Reward I Masalla <reward.masalla@trc.co.tz>
2023-01-18 14:09:17 +0300

    finalizing oauth

---
+++ 3_OAUTH/walinzi/account/views.py
@@ -0,0 +1,80 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
+# Create your views here.
+from .forms import *
+from .models import Profiles
+from django.contrib import messages
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': dashboard})
+
+
+# this code is added on 6/1/23
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+
+            Profiles.objects.create(user=new_user)
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+
+
+@login_required
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profiles, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated successfully.')
+        else:
+            messages.error(request, 'Error updating profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profiles)
+
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
